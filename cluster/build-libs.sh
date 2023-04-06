dfa434ca442f43183d57dd4756ad95d2546d837d
Garth <244253+xgp@users.noreply.github.com>
2023-04-06 22:15:49 +0200

    build-libs trying verbose

--- cluster/build-libs.sh
+++ cluster/build-libs.sh
@@ -2,7 +2,7 @@
 
 function checkout_build {
     echo "Building $1"
-    git clone git@github.com:p2-inc/$1.git
+    git clone --verbose git@github.com:p2-inc/$1.git
     cd $1
     git checkout main && git rev-parse HEAD
     mvn -B clean package
