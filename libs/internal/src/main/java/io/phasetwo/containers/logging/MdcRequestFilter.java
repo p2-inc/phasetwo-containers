ed128ed687ed79ea70cc9fa8af5129c015c749b1
Garth <244253+xgp@users.noreply.github.com>
2025-10-23 13:42:40 +0200

    MDC cluster param (#101)

--- libs/internal/src/main/java/io/phasetwo/containers/logging/MdcRequestFilter.java
+++ libs/internal/src/main/java/io/phasetwo/containers/logging/MdcRequestFilter.java
@@ -5,6 +5,7 @@ import jakarta.ws.rs.container.ContainerRequestContext;
 import jakarta.ws.rs.container.ContainerRequestFilter;
 import jakarta.ws.rs.ext.Provider;
 import java.io.IOException;
+import java.util.Optional;
 import lombok.extern.jbosslog.JBossLog;
 import org.jboss.logging.MDC;
 import org.keycloak.models.KeycloakSession;
@@ -27,5 +28,14 @@ public class MdcRequestFilter implements ContainerRequestFilter {
       String realmName = session.getContext().getRealm().getName();
       MDC.put("realm", realmName);
     }
+    getCluster().ifPresent(cluster -> {
+        MDC.put("cluster", cluster);
+      });
+  }
+
+  public static final String PHASETWO_CLUSTER_KEY = "PHASETWO_CLUSTER";
+  
+  private static Optional<String> getCluster() {
+    return Optional.ofNullable(System.getenv(PHASETWO_CLUSTER_KEY));
   }
 }
